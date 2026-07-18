9805193907cdb5eb63d55baf58bb9d1929a1d316
CodeWithAman <amanverma78503@gmail.com>
2026-07-18 23:53:52 +0530

    Add authentication middleware for user and admin access control

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+
+
+// This middleware is simply used to check user login or not 
+export const AuthProtect = async (req, res, next) => {
+  let token = req.headers.authorization;
+  if (token && token.startsWith("Bearer")) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      if (!req.user) {
+        return res
+          .status(401)
+          .json({ message: "Not authorized, token failed" });
+      }
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "Not authorized, token failed" });
+    } 
+  } else {
+    return res.status(401).json({ message: "Not authorized, token failed" });
+  }
+};
+
+
+export const AdminAuthProtect = (req, res, next) => {
+    if(req.user && req.user.role === 'admin'){
+        next();
+    }else{
+        return res.status(403).json({message: 'Forbidden, admin access required'})
+    }
+}
+
